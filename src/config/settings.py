031158eb94d2b528675881303bffd9ab2a9080b7
Damian Wojciechowski <damian.wojciechowski@leverx.com>
2026-02-19 15:22:27 +0400

    Switch from sqlite to postgresql

--- src/config/settings.py
+++ src/config/settings.py
@@ -93,10 +93,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    #
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'orders_db',
+        'USER': 'orders_user',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
     }
 }
 
